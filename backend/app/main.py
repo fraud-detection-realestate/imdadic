74dadcdaee8ef80e5ae25be5d7e69856b540fd90
DYLBER CABRERA <dylbercabrera23@hotmail.com>
2025-11-25 21:36:55 -0500

    feat: conexion con  (Gemini, FastAPI)

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.api.v1.chat import router as chat_router
+
+app = FastAPI(debug=True)
+
+# Registrar rutas
+app.include_router(chat_router, prefix="/api/v1", tags=["Chat"])
+
+
+@app.get("/")
+async def root():
+    return {"message": "API is running"}
+                         
---
+++ backend/app/schemas/chat.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+#Pydantic model for request body
+class ChatRequest(BaseModel):
+    message: str # nosotros esperamos un cuerpo JSON como {"prompt": "tu mensaje aqui"}
+    
+class ChatResponse(BaseModel):
+    response: str # Devolvemos un cuerpo JSON como {"response": "respuesta del chat aqui"}
+    
